7658366d46ac64a7d7ce833b07207c568a35c3dc
echoykx <83965853+echoykx@users.noreply.github.com>
2021-05-11 00:47:10 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("github test");
     }
 }
